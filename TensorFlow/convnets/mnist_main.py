978474c875e3b3212daee91abfe199a0b8146756
Robert Grzelka <robert.grzelka@outlook.com>
2018-12-08 12:29:46 +0100

    .

--- TensorFlow/convnets/mnist_main.py
+++ TensorFlow/convnets/mnist_main.py
@@ -7,11 +7,11 @@
 def ipython_info():
 	import sys
 	ip = False
-	print(sys.modules)
 	if 'ipykernel' in sys.modules:
 		ip = True  # 'notebook'
 	# elif 'IPython' in sys.modules:
 	#    ip = 'terminal'
+	print(sys.modules)
 	return ip
 
 
