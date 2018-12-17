a58b104e6d58369f45e64f7b830975a19f0795f7
Robert Grzelka <robert.grzelka@outlook.com>
2018-12-17 01:24:46 +0100

    .

---
+++ Ikmnet/Python3 Programming/main1.py
@@ -0,0 +1,6 @@
+
+
+from package.module1 import *
+
+print (vars())
+print (globals())
---
+++ Ikmnet/Python3 Programming/package/module1.py
@@ -0,0 +1,10 @@
+__all__ = ['A','B','C']
+
+class A(object):
+
+    pass
+
+class B(object): pass
+class C(object): pass
+class D(object): pass
+
